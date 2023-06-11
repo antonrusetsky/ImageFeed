a1166c1050f30376f10556784dc912d255c65bc7
Anton Rusetsky <antonrusetsky@icloud.com>
2023-06-11 15:19:32 +0700

    some fixes

--- ImageFeed/ImagesList/ImagesListService.swift
+++ ImageFeed/ImagesList/ImagesListService.swift
@@ -8,7 +8,7 @@
 import UIKit
 import Kingfisher
 
-class ImagesListService {
+final class ImagesListService {
     private (set) var photos: [Photo] = []
     
     private var lastLoadedPage: Int?
@@ -20,7 +20,7 @@ class ImagesListService {
 
     func fetchPhotosNextPage() {
         guard task == nil else {return}
-        let nextPage = lastLoadedPage == nil ? 1 : lastLoadedPage! + 1
+        let nextPage = (lastLoadedPage ?? 0) + 1 
         
         var urlComponents = URLComponents(string: "https://api.unsplash.com")
         urlComponents?.path = "/photos"
@@ -115,53 +115,3 @@ class ImagesListService {
         dataTask.resume()
     }
 }
-
-struct Photo {
-    let id: String
-    let size: CGSize
-    let createdAt: Date?
-    let welcomeDescription: String?
-    let thumbImageURL: String
-    let largeImageURL: String
-    let likedByUser: Bool
-}
-
-struct PhotoResult: Codable {
-    let id: String
-    let width: Int
-    let height: Int
-    let createdAt: String?
-    let description: String?
-    let urls: UrlsResult
-    let likedByUser: Bool
-    
-    enum CodingKeys: String, CodingKey {
-        case id
-        case createdAt = "created_at"
-        case width
-        case height
-        case description
-        case urls
-        case likedByUser = "liked_by_user"
-    }
-}
-
-struct UrlsResult: Codable {
-    let raw: String
-    let full: String
-    let regular: String
-    let small: String
-    let thumb: String
-}
-
-struct LikeResult: Codable {
-    let photo: PhotoResult
-    let user: User
-}
-
-struct User: Codable {
-    let id: String
-    let username: String
-    let name: String
-}
-
